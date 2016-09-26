d03dd71112fbcdfe6f3baa6eff6cc8f6f7011c48
yunsik <wkdrn008@gmail.com>
2016-09-26 14:55:36 +0900

    ㄴㅅ

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,11 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("테스트");
-<<<<<<< HEAD
 		System.out.println("ㄹㄷㄹㅈㄷㄹㅈㄷㄹ");
-=======
 		System.out.println("테스트2 박지원 충돌내기");
->>>>>>> refs/remotes/choiwj1012/master
+
 		
 	}
 	
